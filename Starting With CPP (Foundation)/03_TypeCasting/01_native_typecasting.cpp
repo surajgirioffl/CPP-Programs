d99b5939aa9512047527e6980444999fe2a9f010
surajgirioffl <surajkumargiri2002@gmail.com>
2022-06-01 21:57:19 +0530

    commit repository for first time

---
+++ Starting With CPP (Foundation)/03_TypeCasting/01_native_typecasting.cpp
@@ -0,0 +1,28 @@
+﻿/*native or traditional explicit typecasting*/
+#include <iostream>
+using namespace std;
+class typeCasting
+{
+    int numInt;
+    float numFloat;
+    char ch;
+
+public:
+    void test()
+    {
+        numInt = 88;
+        numFloat = 20.5400;
+        ch = 'B';
+        char ch2;
+        cout << "write a character" << endl;
+        cin.get(ch2);
+
+        cout << (char)numInt << endl;
+    }
+};
+
+int main()
+{
+    typeCasting obj;
+    obj.test();
+}
---
+++ Starting With CPP (Foundation)/04_Functions/01_pass_by_value.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+using namespace std;
+void swap(int, int); // declaration
+int main()
+{
+    system("cls");
+    int a = 5;
+    int b = 10;
+
+    cout << "In function main()" << ends
+         << "a = " << a << " & b = " << b << endl;
+
+    swap(a, b);
+    cout << "In function main() after return from swap()"
+         << "a = " << a << " & b = " << b << endl;
+    return 0;
+}
+
+void swap(int x, int y) // we can also use a,b because it's local variable.
+{
+    int temp = x;
+    x = y;
+    y = temp;
+    cout << "In function swap()"
+         << "a = " << x << " & b = " << y << endl;
+}
+/*
+ *It is clear from above program that the value of a and b copied to x and y.
+ *We swap the x and y in function swap() and it's doesn't affect the original value.
+ *So,In pass by value the original data remains unaffected.
+ */
