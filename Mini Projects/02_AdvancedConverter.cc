eecd3e58885bb04f15c3f433d1d166a8377be931
surajgirioffl <surajkumargiri2002@gmail.com>
2022-06-20 09:57:06 +0530

    added some incomplete projects

---
+++ Mini Projects/02_AdvancedConverter.cc
@@ -0,0 +1,14 @@
+/*
+ **ADVANCED CALCULATOR
+ *1. BMI
+ *2. Data
+ *3. Length
+ *4. Area
+ *5. Volume
+ *6. Temperature
+ *7. Speed
+ *8. Time
+ *9. Mass
+ *10. Number System (binary, octal, decimal, hexadecimal and more...)
+ *11. Currency
+ */
---
+++ Mini Projects/03_Tic_Tac_Toe.cc
@@ -0,0 +1,14 @@
+/*Tic-Tac-Toe Game using C++*/
+/**
+ *author: Suraj Kumar Giri.
+ *Date: 19-06-2022
+ *Time: 00:03:06
+ */
+
+/*
+ *
+ */
+
+#include <iostream>
+using namespace std;
+
